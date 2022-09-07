ec35fbf492b69fd6015bf8be9be8e0bc8c590d70
23dgupta <durgeshkumar91g@gmail.com>
2022-09-07 14:04:55 +0530

    h2 db implemented

--- src/main/java/com/durgesh/model/Country.java
+++ src/main/java/com/durgesh/model/Country.java
@@ -2,6 +2,7 @@ package com.durgesh.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -11,6 +12,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name="Country")
 public class Country {
 	@Id
 	int id;
---
+++ src/main/resources/data.sql
@@ -0,0 +1,3 @@
+insert into country values(1,'Delhi','India')
+insert into country values(2,'Washington','USA')
+insert into country values(3,'London','Uk')
