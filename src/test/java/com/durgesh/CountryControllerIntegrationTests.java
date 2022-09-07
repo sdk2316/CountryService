91cb741f4b9a9b908e27bb98b2ceff7b2a75f8e2
23dgupta <durgeshkumar91g@gmail.com>
2022-09-07 14:45:38 +0530

    AddCountry integration test case completed using RestTemplate

--- src/test/java/com/durgesh/CountryControllerIntegrationTests.java
+++ src/test/java/com/durgesh/CountryControllerIntegrationTests.java
@@ -1,83 +1,94 @@
 package com.durgesh;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.json.JSONException;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
 import org.skyscreamer.jsonassert.JSONAssert;
-import org.json.JSONException;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
+
+import com.durgesh.model.Country;
 
 @SpringBootTest
 @TestMethodOrder(OrderAnnotation.class)
 public class CountryControllerIntegrationTests {
-	
-	
+
 	@Test
 	@Order(1)
 	void getAllContriesIntegrationTests() throws JSONException {
-		String expected="[\r\n" + 
-				"    {\r\n" + 
-				"        \"id\": 1,\r\n" + 
-				"        \"countryName\": \"India\",\r\n" + 
-				"        \"countryCapital\": \"Delhi\"\r\n" + 
-				"    },\r\n" + 
-				"    {\r\n" + 
-				"        \"id\": 2,\r\n" + 
-				"        \"countryName\": \"USA\",\r\n" + 
-				"        \"countryCapital\": \"Washington\"\r\n" + 
-				"    },\r\n" + 
-				"    {\r\n" + 
-				"        \"id\": 3,\r\n" + 
-				"        \"countryName\": \"Uk\",\r\n" + 
-				"        \"countryCapital\": \"London\"\r\n" + 
-				"    }\r\n" + 
-				"]";
-		RestTemplate restTemplate=new RestTemplate();
-		
+		String expected = "[\r\n" + "    {\r\n" + "        \"id\": 1,\r\n" + "        \"countryName\": \"India\",\r\n"
+				+ "        \"countryCapital\": \"Delhi\"\r\n" + "    },\r\n" + "    {\r\n" + "        \"id\": 2,\r\n"
+				+ "        \"countryName\": \"USA\",\r\n" + "        \"countryCapital\": \"Washington\"\r\n"
+				+ "    },\r\n" + "    {\r\n" + "        \"id\": 3,\r\n" + "        \"countryName\": \"Uk\",\r\n"
+				+ "        \"countryCapital\": \"London\"\r\n" + "    }\r\n" + "]";
+		RestTemplate restTemplate = new RestTemplate();
+
 		ResponseEntity<String> res = restTemplate.getForEntity("http://localhost:8877/getcountries", String.class);
-		
+
 		System.out.println(res.getBody());
 		System.out.println(res.getStatusCode());
-		JSONAssert.assertEquals(expected,res.getBody(), false);
+		JSONAssert.assertEquals(expected, res.getBody(), false);
 	}
-	
-	
+
 	@Test
 	@Order(2)
 	void getAllContryByIdIntegrationTests() throws JSONException {
-		String expected="{\r\n" + 
-				"    \"id\": 1,\r\n" + 
-				"    \"countryName\": \"India\",\r\n" + 
-				"    \"countryCapital\": \"Delhi\"\r\n" + 
-				"}";
-		RestTemplate restTemplate=new RestTemplate();
-		
-		ResponseEntity<String> res = restTemplate.getForEntity("http://localhost:8877/getcountriesById/1", String.class);
-		
+		String expected = "{\r\n" + "    \"id\": 1,\r\n" + "    \"countryName\": \"India\",\r\n"
+				+ "    \"countryCapital\": \"Delhi\"\r\n" + "}";
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<String> res = restTemplate.getForEntity("http://localhost:8877/getcountriesById/1",
+				String.class);
+
 		System.out.println(res.getBody());
 		System.out.println(res.getStatusCode());
-		JSONAssert.assertEquals(expected,res.getBody(), false);
+		JSONAssert.assertEquals(expected, res.getBody(), false);
 	}
-	
-	
+
 	@Test
 	@Order(2)
 	void getAllContryByNameIntegrationTests() throws JSONException {
-		String expected="{\r\n" + 
-				"    \"id\": 2,\r\n" + 
-				"    \"countryName\": \"USA\",\r\n" + 
-				"    \"countryCapital\": \"Washington\"\r\n" + 
-				"}";
-		RestTemplate restTemplate=new RestTemplate();
-		
-		ResponseEntity<String> res = restTemplate.getForEntity("http://localhost:8877/getcountries/countryname?name=USA", String.class);
-		
+		String expected = "{\r\n" + "    \"id\": 2,\r\n" + "    \"countryName\": \"USA\",\r\n"
+				+ "    \"countryCapital\": \"Washington\"\r\n" + "}";
+		RestTemplate restTemplate = new RestTemplate();
+
+		ResponseEntity<String> res = restTemplate
+				.getForEntity("http://localhost:8877/getcountries/countryname?name=USA", String.class);
+
 		System.out.println(res.getBody());
 		System.out.println(res.getStatusCode());
-		JSONAssert.assertEquals(expected,res.getBody(), false);
+		JSONAssert.assertEquals(expected, res.getBody(), false);
 	}
 
+	@Test
+	@Order(4)
+	public void test_AddCountryIntegrationTest() throws Exception {
+
+		Country country = new Country(8, "Germany", "Dutch & Uk");
+
+		String expected = "{\r\n" + "    \"id\": 8,\r\n" + "    \"countryName\": \"Germany\",\r\n"
+				+ "    \"countryCapital\": \"Dutch & Uk\"\r\n" + "}";
+
+		RestTemplate restTemplate = new RestTemplate();
+		HttpHeaders headers = new HttpHeaders();
+		headers.setContentType(MediaType.APPLICATION_JSON);
+		HttpEntity<Country> request = new HttpEntity<Country>(country, headers);
+
+		ResponseEntity<String> res = restTemplate.postForEntity("http://localhost:8877/addCountry",request, String.class);
+
+		System.out.println(res.getBody());
+		System.out.println(res.getStatusCode());
+		
+		assertEquals(HttpStatus.CREATED,res.getStatusCode());
+		JSONAssert.assertEquals(expected, res.getBody(), false);
+	}
 }
